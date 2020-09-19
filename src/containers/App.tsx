2539a302879631536bc776e35bac633b49976d64
ianeli1 <ianelicha@gmail.com>
2020-09-19 16:33:24 -0600

    Users can now register and log in

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -13,6 +13,7 @@ import {
   Theme,
   Hidden,
   Typography,
+  Button,
 } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
 import NewsFeed from "./feed";
@@ -20,6 +21,10 @@ import * as r from "../misc/reference";
 import { LeftSidebar } from "./leftSidebar";
 import Profile from "./profile";
 import { connect } from "react-redux";
+import { Login } from "../components/Login";
+import { createNewUser, login } from "../misc/firebaseMgmt";
+import { Register } from "../components/Register";
+import { UserData } from "../components";
 
 const AppCSS = (theme: Theme) =>
   createStyles({
@@ -35,18 +40,14 @@ const AppCSS = (theme: Theme) =>
   });
 
 type AppState = {
+  user: r.User | null;
   mobileOpen: boolean;
+  loggedIn: boolean;
+  showLogin: boolean;
+  showRegister: boolean;
 };
 interface AppProps extends WithStyles<typeof AppCSS> {
-  user: r.User;
-  /*
-  currentUser: r.User;
-  currentProfile: r.Profile | null;
-  feed: r.Feed | null;
-  users: {
-    [key: string]: r.User;
-  };
-  ref: r.Feed;*/
+  user: r.User | null;
 }
 
 /*
@@ -66,14 +67,37 @@ class App extends Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = {
+      user: null,
       mobileOpen: false,
+      loggedIn: Boolean(this.props.user),
+      showLogin: false,
+      showRegister: true,
     };
+  }
+
+  componentDidUpdate() {
+    if (
+      !this.state.user ||
+      (this.props.user && this.state.user.userId != this.props.user.userId)
+    ) {
+      this.setState({ user: this.props.user });
+    }
   }
 
   render() {
     return (
       <div className={this.props.classes.outer}>
         <CssBaseline />
+        <Login
+          login={login}
+          close={() => this.setState({ showLogin: false })}
+          open={this.state.showLogin}
+        />
+        <Register
+          open={this.state.showRegister}
+          close={() => this.setState({ showRegister: false })}
+          createUser={createNewUser}
+        />
         <Box className={this.props.classes.root}>
           <LeftSidebar
             mobileOpen={this.state.mobileOpen}
@@ -83,15 +107,22 @@ class App extends Component<AppProps, AppState> {
             }}
             setMobileOpen={() => this.setState({ mobileOpen: false })}
           >
-            <AppToolbar setMenu={() => this.setState({ mobileOpen: true })} />
+            <AppToolbar
+              user={this.state.user}
+              setMenu={() => this.setState({ mobileOpen: true })}
+              openLogin={() => this.setState({ showLogin: true })}
+              openRegister={() => this.setState({ showRegister: true })}
+            />
             <Box className={this.props.classes.innerApp}>
               <Switch>
                 <Route exact path="/">
                   <NewsFeed />
                 </Route>
-                <Route exact path="/profile">
-                  <Redirect to={"/profile/" + this.props.user.userId} />
-                </Route>
+                {this.props.user && (
+                  <Route exact path="/profile">
+                    <Redirect to={"/profile/" + this.props.user.userId} />
+                  </Route>
+                )}
 
                 <Route
                   path="/profile/:id"
@@ -108,7 +139,12 @@ class App extends Component<AppProps, AppState> {
   }
 }
 
-function AppToolbar(props: { setMenu: () => void }) {
+function AppToolbar(props: {
+  user: r.User | null;
+  setMenu: () => void;
+  openLogin: () => void;
+  openRegister: () => void;
+}) {
   return (
     <AppBar position="static">
       <Toolbar>
@@ -116,6 +152,15 @@ function AppToolbar(props: { setMenu: () => void }) {
           <IconButton onClick={props.setMenu}>
             <Menu />
           </IconButton>
+          <div style={{ flexGrow: 1 }} />
+          {props.user ? (
+            <UserData user={props.user} />
+          ) : (
+            <div>
+              <Button onClick={props.openLogin}>Login</Button>
+              <Button onClick={props.openLogin}>Register</Button>
+            </div>
+          )}
         </Hidden>
         <Typography variant="h5">Simple-Social</Typography>
       </Toolbar>
